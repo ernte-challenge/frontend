7c987be4990c2446d763164d116113473d47d4d8
Christoph Holewa <christoph.holewa@rewe-digital.com>
2020-03-21 21:57:17 +0100

    add missing import

--- src/components/User/UserProfilePage.tsx
+++ src/components/User/UserProfilePage.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
-import styles from "../../styles";
-import {Button} from "@material-ui/core";
-import Grid from "@material-ui/core/Grid";
-import {REGISTER_PATH} from "../../routes";
+import {LOGIN_PAGE_PATH, REGISTER_PATH} from "../../routes";
 import {Redirect} from 'react-router-dom';
 
 const UserProfilePage = () => {
