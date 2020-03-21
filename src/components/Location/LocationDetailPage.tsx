e45e9ce37ef22e873a651a76850bfd48c17757ca
Christoph Holewa <christoph.holewa@rewe-digital.com>
2020-03-21 21:48:47 +0100

    add fetch for locations

--- src/components/Location/LocationDetailPage.tsx
+++ src/components/Location/LocationDetailPage.tsx
@@ -1,14 +1,49 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {API_MARKETS_PATH} from "../../routes";
+import LocationCard from "./LocationCard";
 
 interface LocationDetailPage {
   locationId: string
 };
 
-const LocationDetailPage = ({ locationId}: LocationDetailPage) => {
-  // TODO: Fetches Location Details by ID
+interface Location {
+  name: string,
+  usersRegistered: number,
+  usersNeeded: number,
+  distance: number,
+  salary: number,
+  imageUrl: string
+};
 
+const LocationDetailPage = ({locationId}: LocationDetailPage) => {
+  const [error, setError] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [locations, setLocations] = useState<Array<Location>>([]);
+
+  useEffect(() => {
+    (async () => {
+      setLoading(true);
+      setError(false);
+      await fetch(API_MARKETS_PATH, {
+        credentials: 'same-origin',
+      })
+        .then(response => response.json())
+        .then(setLocations)
+        .catch(setError);
+    })();
+  }, []);
+
+  if (error) {
+    return <div>Error!</div>
+  }
+  if (loading) {
+    return <div>Loading...</div>
+  }
   return (
-    <div>Location {locationId}</div>
+    locations.map((loc) => (
+      <LocationCard locationName={loc.name} usersRegistered={loc.usersRegistered}
+                    usersNeeded={loc.usersNeeded} distance={loc.distance}
+                    salary={loc.salary} imageUrl={loc.imageUrl}/>))
   );
 };
 
--- src/routes.ts
+++ src/routes.ts
@@ -2,3 +2,5 @@ export const LOCATIONS_LIST_PATH : string = "/locations";
 export const USER_PROFILE_PATH : string = "/profile";
 export const LOCATIONS_MAP_PATH : string = "/locations/map";
 export const REGISTER_PATH : string = "/register";
+
+export const API_MARKETS_PATH : string = "/api/markets";
