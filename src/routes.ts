253a138dd226d4c6428d4fe1e7608dbf6b863dde
Christoph Holewa <christoph.holewa@rewe-digital.com>
2020-03-22 09:41:59 +0100

    fix paths

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,6 @@ export const USER_PROFILE_PATH : string = "/profile";
 export const LOCATIONS_MAP_PATH : string = "/locations/map";
 export const REGISTER_PATH : string = "/register";
 export const LOGIN_PAGE_PATH : string = "/login";
-
+export const FORGOT_PASSWORD_PATH : string = "/";
 export const API_LOCATIONS_PATH : string = "/api/locations";
 export const API_LOGIN_PATH : string = "/api/login";
